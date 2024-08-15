67a2e38680c8c5c62e925060b8afc7bf0ee25e66
Gabriel Trifoni <gabrielsoavetrifoni@gmail.com>
2024-08-15 11:37:36 -0300

    Route parameters

---
+++ src/events.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+
+@Controller('/events')
+export class EventsController {
+    @Get()
+    findAll() {}
+
+    @Get(':id')
+    findOne(@Param('id') id) {
+        return id;
+    }
+
+    @Post()
+    create() {}
+
+    @Patch()
+    update() {}
+
+    @Delete()
+    remove() {}    
+}
