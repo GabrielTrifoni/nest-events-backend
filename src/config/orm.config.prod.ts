2be2defdd77af8ed6ebc95cbf91d37dcc87f7702
Gabriel Trifoni <gabrielsoavetrifoni12@gmail.com>
2024-08-26 14:16:30 -0300

    deploy database on railway instead of docker locally

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -6,12 +6,8 @@ import { Event } from 'src/events/event.entity';
 export default registerAs(
   'orm.config',
   (): TypeOrmModuleOptions => ({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
     entities: [Event, Attendee],
     synchronize: false,
   }),
