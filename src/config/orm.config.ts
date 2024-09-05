228d408116c44a68e7e1ddc98b9b16cd74afe122
Gabriel <gabrielsoavetrifoni12@gmail.com>
2024-09-05 19:57:40 -0300

    change orm.config

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -11,7 +11,7 @@ export default registerAs(
     type: 'mysql',
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
-    username: 'admin',
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: [Event, Attendee, Subject, Teacher],
