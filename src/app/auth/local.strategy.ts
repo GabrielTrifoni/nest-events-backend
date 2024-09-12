df0ca916964d4502b2a1f626a950e211462b1070
Gabriel <gabrielsoavetrifoni12@gmail.com>
2024-09-12 14:37:04 -0300

    generate jwt

--- src/app/auth/local.strategy.ts
+++ src/app/auth/local.strategy.ts
@@ -6,7 +6,7 @@ import { User } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
     private readonly logger = new Logger(LocalStrategy.name);
 
     constructor(
