55bf027569fe546fa639223b926a51090292e980
Gabriel <gabrielsoavetrifoni12@gmail.com>
2024-09-26 18:50:55 -0300

    serializing nested objects

--- src/app/pagination/paginator.ts
+++ src/app/pagination/paginator.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import { SelectQueryBuilder } from 'typeorm';
 
 export interface PaginateOptions {
@@ -6,11 +7,20 @@ export interface PaginateOptions {
   total?: boolean;
 }
 
-export interface PaginationResult<T> {
+export class PaginationResult<T> {
+  constructor(partial: Partial<PaginationResult<T>>) {
+    Object.assign(this, partial);
+  }
+
+  @Expose()
   first: number;
+  @Expose()
   last: number;
+  @Expose()
   limit: number;
+  @Expose()
   total?: number;
+  @Expose()
   data: T[];
 }
 
@@ -21,14 +31,14 @@ export async function paginate<T>(
     currentPage: 1,
   },
 ): Promise<PaginationResult<T>> {
-    const offset = (options.currentPage - 1) * options.limit;
-    const data = await qb.limit(options.limit).offset(offset).getMany();
+  const offset = (options.currentPage - 1) * options.limit;
+  const data = await qb.limit(options.limit).offset(offset).getMany();
 
-    return {
-        first: offset + 1,
-        last: offset + data.length,
-        limit: options.limit,
-        total: options.total ? await qb.getCount() : null,
-        data
-    }
+  return new PaginationResult({
+    first: offset + 1,
+    last: offset + data.length,
+    limit: options.limit,
+    total: options.total ? await qb.getCount() : null,
+    data
+  })
 }
